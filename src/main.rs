cafc8dd23bb45abc3e7169d08ab5e49fe901f685
alire-alavi <alirezaalavirazavi@yahoo.com>
2025-04-03 23:20:00 +0330

    Refactor hello endpoint into separate handler function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-#![allow(unused)]
-
 use std::net::SocketAddr;
+use serde::Deserialize;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{response::{Html, IntoResponse}, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(|| async { Html("Hello <strong> World!</strong>")}));
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("--> LISTENING on {addr}\n");
     axum::serve(TcpListener::bind(addr).await.unwrap(), routes_hello).await.unwrap();
+}
+
+async fn handler_hello() ->  impl IntoResponse {
+    println!("->> {:?} - handler_hello", "HANDLER");
+    Html("Hello <strong>world</strong>")
 }
