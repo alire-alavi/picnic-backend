33de36c4b8508d3e68dffba6c9a5328292779fee
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-05 21:59:34 +0330

    feat: integrate Apollo Server and GraphQL with NestJS
    - Added dependencies for Apollo Server, NestJS Apollo, GraphQL, class-transformer, and class-validator in package.json.
    - Configured GraphQLModule in app.module.ts with ApolloDriver, enabling auto schema generation and context handling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Request, Response } from 'express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
@@ -9,6 +12,12 @@ import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+      playground: true,
+      context: ({ req, res }: { req: Request; res: Response }) => ({ req, res }),
+    }),
     HealthModule,
     ProductsModule,
     PrismaModule,
