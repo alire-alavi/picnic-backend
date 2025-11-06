50f9ba33425b7ec370b50e5affd5e6e96300e078
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-06 16:48:13 +0330

    feat: enhance ProductsService to support filtering and pagination in list method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { Product } from '@prisma/client';
+import { Injectable } from '@nestjs/common'
+import { Product } from '@prisma/client'
+import { PrismaService } from '../prisma/prisma.service'
+import { ProductsFilter } from './dto/products.interface'
 
 @Injectable()
 export class ProductsService {
     constructor(private readonly prisma: PrismaService) { }
 
-    async list(): Promise<Product[]> {
+    async list(filter?: ProductsFilter, limit?: number): Promise<Product[]> {
         return this.prisma.product.findMany({
-            orderBy: { createdAt: 'desc' }
-        });
+            orderBy: { createdAt: 'desc' },
+            where: filter ? { categoryId: filter.category } : {},
+            take: limit || 20,
+        })
+    }
+
+    async getById(id: string): Promise<Product | null> {
+        return this.prisma.product.findUnique({ where: { id } })
     }
 }
