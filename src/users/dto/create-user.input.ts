f42889746c26c5286780201924af1bcd4d24228b
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-18 17:59:47 +0330

    refactor: remove unused ValidateIf decorator from CreateUserInput and enhance password field description

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,5 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql'
-import { IsEmail, IsOptional, IsString, MinLength, ValidateIf } from 'class-validator'
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator'
 
 @InputType()
 export class CreateUserInput {
@@ -18,9 +18,8 @@ export class CreateUserInput {
     @IsString()
     name?: string
 
-    @Field({ nullable: true })
+    @Field({ nullable: true, description: 'Password is optional. Can be set later using setPassword mutation.' })
     @IsOptional()
-    @ValidateIf(o => !!o.email) // password required only if email present
-    @MinLength(8)
+    @MinLength(8, { message: 'Password must be at least 8 characters long' })
     password?: string
 }
