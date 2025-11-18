ad68a2f3194f3c76295fea5ce9e75db69e8860e4
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-18 17:58:36 +0330

    add: integrate CacheModule with Redis options in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
+import { Module } from '@nestjs/common'
+import { CacheModule } from '@nestjs/cache-manager'
+import { UsersService } from './users.service'
+import { UsersResolver } from './users.resolver'
+import { UsersController } from './users.controller'
+import { RedisOptions } from '../configs/redis-config'
 
 @Module({
+  imports: [CacheModule.registerAsync(RedisOptions)],
+  controllers: [UsersController],
   providers: [UsersService, UsersResolver],
   exports: [UsersService],
 })
