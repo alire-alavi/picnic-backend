6be7897be1a93ed92acee9e9dc18399bec26cec9
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-11-18 18:04:16 +0330

    refactor: enhance AuthModule by adding UsersModule and AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtStrategy } from './strategies/jwt.strategy'
 import { AuthResolver } from './auth.resolver'
+import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { CacheModule } from '@nestjs/cache-manager'
 import { RedisOptions } from '../configs/redis-config'
+import { UsersModule } from '../users/users.module'
 
 @Module({
   imports: [
     ConfigModule,
     CacheModule.registerAsync(RedisOptions),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    forwardRef(() => UsersModule),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -29,7 +32,8 @@ import { RedisOptions } from '../configs/redis-config'
       }),
     }),
   ],
+  controllers: [AuthController],
   providers: [JwtStrategy, AuthResolver, AuthService],
-  exports: [JwtModule, PassportModule],
+  exports: [JwtModule, PassportModule, AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
