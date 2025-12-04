1088fe1ba6227c4c7a30d167e7cdae978709c8aa
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-12-04 16:21:05 +0330

    update: modify storage module, remove image creation code

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -2,12 +2,11 @@ import { Global, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { StorageService } from './storage.service'
 import { StorageController } from './storage.controller'
-import { StorageResolver } from './storage.resolver'
 
 @Global()
 @Module({
     imports: [ConfigModule],
-    providers: [StorageService, StorageResolver],
+    providers: [StorageService],
     controllers: [StorageController],
     exports: [StorageService],
 })
